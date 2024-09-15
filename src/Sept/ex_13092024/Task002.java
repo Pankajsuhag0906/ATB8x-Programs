a21c45b3c1bbca85ba83c5341ee311560fb8e7ad
Pankajsuhag0906 <pankajsuhag494@gmail.com>
2024-09-15 14:42:20 +0530

    Full commit

---
+++ src/Sept/ex_13092024/Task002.java
@@ -0,0 +1,25 @@
+package Sept.ex_13092024;
+
+import java.util.Scanner;
+
+public class Task002 {
+    public static void main(String[] args) {
+        System.out.print("Enter first Number = ");
+        System.out.println();
+        Scanner obj1=new Scanner(System.in);
+        double num1=obj1.nextDouble();
+        System.out.print("Enter Second Number = ");
+        System.out.println();
+        Scanner obj2=new Scanner(System.in);
+        double num2=obj2.nextDouble();
+
+        System.out.printf("%f + %f = %f ",num1,num2,num1+num2);
+        System.out.println();
+        System.out.printf(" %f - %f = %f",num1,num2,num1-num2);
+        System.out.println();
+        System.out.printf(" %f / %f = %f ",num1,num2,num1/num2);
+        System.out.println();
+        System.out.printf(" %f Modulus %f = %f ",num1,num2,num1%num2);
+
+    }
+}
