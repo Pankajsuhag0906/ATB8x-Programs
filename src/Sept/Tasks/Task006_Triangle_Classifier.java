f00d06b118bfb01ca6babfcd9d19fd175df4cf7c
Pankajsuhag0906 <pankajsuhag494@gmail.com>
2024-09-22 21:20:02 +0530

    Full commit

---
+++ src/Sept/Tasks/Task006_Triangle_Classifier.java
@@ -0,0 +1,29 @@
+package Sept.Tasks;
+
+import java.util.Scanner;
+
+public class Task006_Triangle_Classifier {
+    public static void main(String[] args) {
+        System.out.println("Enter Length of First Side: ");
+        Scanner obj1=new Scanner(System.in);
+        int Side1= obj1.nextInt();
+        System.out.println("Enter Length of Second Side: ");
+        Scanner obj2=new Scanner(System.in);
+        int Side2= obj2.nextInt();
+        System.out.println("Enter Length of Third Side: ");
+        Scanner obj3=new Scanner(System.in);
+        int Side3= obj3.nextInt();
+
+        if(Side1==Side2 && Side1==Side3){
+            System.out.println("This is an Equilateral Triangle");
+        }
+        else if (Side1==Side2 || Side1==Side3|| Side2==Side3) {
+            System.out.println("This is an Isosceles Triangle");
+        }
+        else{
+            System.out.println("This is an Scalene Triangle");
+        }
+
+
+    }
+}
