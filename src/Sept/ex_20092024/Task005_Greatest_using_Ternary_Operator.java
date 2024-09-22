77af86dc7c2f621b3905d7dee535c5280bf38904
Pankajsuhag0906 <pankajsuhag494@gmail.com>
2024-09-22 21:12:52 +0530

    Full commit

---
+++ src/Sept/ex_20092024/Task005_Greatest_using_Ternary_Operator.java
@@ -0,0 +1,26 @@
+package Sept.ex_20092024;
+
+import java.util.Scanner;
+
+public class Task005_Greatest_using_Ternary_Operator {
+    public static void main(String[] args) {
+       /* By using Ternary Operators max between 3 numbers.
+         Input int - a,b,c - a = 10, b = 20, c = 45 */
+
+        System.out.println("Enter First Number: ");
+        Scanner obj1=new Scanner(System.in);
+        int A= obj1.nextInt();
+        System.out.println("Enter Second Number: ");
+        Scanner obj2=new Scanner(System.in);
+        int B= obj2.nextInt();
+        System.out.println("Enter Third Number: ");
+        Scanner obj3=new Scanner(System.in);
+        int C= obj3.nextInt();
+
+
+        int max= A>B?(A>C?A:C):(B>C?B:C);
+        System.out.println(max + " is Greater");
+
+
+    }
+}
---
+++ src/Sept/ex_20092024/Task008_Find_LeapYear.java
@@ -0,0 +1,23 @@
+package Sept.ex_20092024;
+
+import java.util.Scanner;
+
+public class Task008_Find_LeapYear {
+
+   // Leap Year Program - Find 2024
+
+    public static void main(String[] args) {
+        System.out.println("Enter the Year :- ");
+        Scanner obj=new Scanner(System.in);
+        int year=obj.nextInt();
+
+        if(year%4==0 || year%400==0){
+            System.out.println(year + " is a leap year");
+        }
+        else{
+
+            System.out.println(year + " is a not leap year");
+        }
+
+    }
+}
