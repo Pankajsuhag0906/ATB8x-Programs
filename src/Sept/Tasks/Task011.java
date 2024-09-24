6716f5ed38e61cd343f6dcd37a98b1140cf68c6b
Pankajsuhag0906 <pankajsuhag494@gmail.com>
2024-09-24 21:09:00 +0530

    Full commit

---
+++ src/Sept/Tasks/Task011.java
@@ -0,0 +1,38 @@
+package Sept.Tasks;
+
+import java.util.Scanner;
+
+public class Task011 {
+    /* Write a program that converts between different units (e.g., kilometers to miles, Celsius to Fahrenheit) based on user selection using a switch statement.
+            Input. -
+    choice - 1 - km → m, km → 1km
+    choice - 2 - f → c, f → c */
+
+    public static void main(String[] args) {
+        System.out.println("Press 1 to Convert Kilometers to Miles");
+        System.out.println("Press 2 to Convert Celsius to Fahrenheit");
+
+        System.out.println("Enter your Choice");
+        Scanner ch=new Scanner(System.in);
+        int choice=ch.nextInt();
+
+        switch (choice){
+            case 1:
+                System.out.println("Enter Kilometers");
+                Scanner k=new Scanner(System.in);
+                double km=k.nextDouble();
+
+                double miles=km*0.621371;
+                System.out.printf("Miles = %f",miles);
+                break;
+
+            case 2:
+                System.out.println("Enter Celsius ");
+                Scanner Cel=new Scanner(System.in);
+                double Celsius=Cel.nextDouble();
+
+                double fahrenhiet= (Celsius * 9/5) + 32;
+                System.out.printf("Fahrenhiet= %f",fahrenhiet);
+        }
+    }
+}
---
+++ src/Sept/ex_22092024/Task009.java
@@ -0,0 +1,60 @@
+package Sept.ex_22092024;
+
+import com.sun.source.util.SourcePositions;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Task009 {
+
+    /* Create a simple calculator that performs addition, subtraction, multiplication, and division, modus based on user input using switch statements.
+      Inputs :   num 1, num 2, +
+      Output :  num1+num2 → print information.
+   */
+    public static void main(String[] args) {
+
+        System.out.println("Press 1 for Addition");
+        System.out.println("Press 2 for Substraction");
+        System.out.println("Press 3 for Multiplication");
+        System.out.println("Press 4 for Division");
+        System.out.println("Press 5 for Modulus");
+
+        System.out.println("Enter your Choice: ");
+        Scanner ch=new Scanner(System.in);
+        int Choice=ch.nextInt();
+
+        System.out.println("Enter First Number: ");
+        Scanner obj1=new Scanner(System.in);
+        double num1=obj1.nextDouble();
+
+        System.out.println("Enter Second Number: ");
+        Scanner obj2=new Scanner(System.in);
+        double num2=obj1.nextDouble();
+        switch (Choice){
+
+            case 1:
+                System.out.printf("%f + %f = %f",num1,num2,num1+num2);
+                break;
+
+            case 2:
+                System.out.printf("%f - %f = %f",num1,num2,num1-num2);
+                break;
+
+            case 3:
+                System.out.printf("%f * %f = %f",num1,num2,num1*num2);
+                break;
+
+            case 4:
+                System.out.printf("%f / %f = %f",num1,num2,num1/num2);
+                break;
+
+            case 5:
+                System.out.printf("%f modulus %f = %f",num1,num2,num1%num2);
+                break;
+        }
+
+
+    }
+
+}
+
