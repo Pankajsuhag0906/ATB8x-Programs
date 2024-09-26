403d8a56aa20be8d593060b9c257ff6f282c4f0f
Pankajsuhag0906 <pankajsuhag494@gmail.com>
2024-09-26 14:10:17 +0530

    Full commit

---
+++ src/Sept/Tasks/Task014.java
@@ -0,0 +1,22 @@
+package Sept.Tasks;
+
+public class Task014 {
+    public static void main(String[] args) {
+        // Prime Number from 1 to 100. (for loop)
+
+
+        for(int i=1; i<=100;i++)
+        {
+            int count=0;
+            for(int j=1;j<=i/2;j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if(count==1)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
---
+++ src/Sept/Tasks/Task015.java
@@ -0,0 +1,27 @@
+package Sept.Tasks;
+
+import java.util.Scanner;
+
+public class Task015 {
+    public static void main(String[] args) {
+        // Count vowels and consonants in a String. (pramod) - v = 2 , c =. 4
+
+        System.out.println("Enter your String: ");
+        Scanner obj=new Scanner(System.in);
+        String str= obj.next();
+        int len= str.length();
+
+        int count=0;
+       for(int i=0;i<len;i++)
+        {
+
+            if(str.charAt(i)=='a' || str.charAt(i)=='e'||str.charAt(i)=='i' || str.charAt(i)=='o'||str.charAt(i)=='u')
+            {
+                  count++;
+            }
+        }
+        System.out.printf("Vowels in %s = %d",str,count);
+
+
+    }
+}
